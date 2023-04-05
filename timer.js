d94487126576d2c84bc5fafbbe76c5669dca9183
orangeman12 <karos2646@gmail.com>
2023-04-05 17:35:16 +0430

    deleted

--- timer.js
+++
@@ -1,7 +0,0 @@
-var start = Date.now();
-setInterval(function() {
-    var delta = Date.now() - start; // milliseconds elapsed since start
-    output(Math.floor(delta / 1000)); // in seconds
-    // alternatively just show wall clock time:
-    output(new Date().toUTCString());
-}, 1000); // update about every second
